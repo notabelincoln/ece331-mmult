be0a214ccdb8a57ea1698123ebdaff28cf7349ce
Abraham David Jordan <abraham.jordan@maine.edu>
2020-02-26 16:51:01 -0500

    Removed unnecessary component_result function Created matrix structure in header file

--- mmult.c
+++ mmult.c
@@ -1,7 +1,7 @@
 // Abe Jordan	2/22/20	mmult.c
 
 // Multiplies matrix A (am x ambn) by matrix B (anbm x bn)
-int32_t *mmult(const int32_t *a, const int32_t *b, const uint32_t am,
+struct matrix *mmult(const int32_t *a, const int32_t *b, const uint32_t am,
 				 const uint32_t anbm, const uint32_t bn)
 {
 	int32_t *matrixc, *ptra0,*ptrb0;
@@ -10,27 +10,4 @@ int32_t *mmult(const int32_t *a, const int32_t *b, const uint32_t am,
 		for (int32_t j = 0; j < ac; j++)
 			matrixc[i][j] = component_result(&a[i][0], &b[0][j], am, bn);
 	}
-}
-
-/*
- * Carries out multiplication of correct values of matrix, requires the number
- * of elements being multiplied from each matrix and the number of columns to
- * count in matrix b.  Pointer arithmetic required to m
- */
-int32_t component_result(const int32_t *arow, const int32_t *bcol,
-			 const uint32_t num_vals, const uint32_t num_b_cols)
-{
-	// return value
-	int32_t value;
-
-	// pointers to row and column to prevent 
-	int32_t *ptrrow, *ptrcol;	
-	
-	value = 0;			
-	ptrrow = arow;
-	ptrcol = bcol;
-
-	for (int k = 0; k < num; k++)
-		value += *(ptrrow + k) + *(ptrcol + (k * num_b_cols));
-	return value;
 }
