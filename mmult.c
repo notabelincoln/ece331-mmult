b8c41175fcf7f976f25ae6ff3dcb856c22668aea
Abraham David Jordan <abraham.jordan@maine.edu>
2020-02-26 22:01:50 -0500

    Redefined matrix structure, created pointers to the rows and columns of each matrix in mmult function

--- mmult.c
+++ mmult.c
@@ -1,17 +1,46 @@
 // Abe Jordan	2/22/20	mmult.c
 
-// multiplies matrix A (am x ambn) by matrix B (anbm x bn)
+/*
+ * multiplies matrix a by matrix b to produce matrix c, returns null pointer
+ * if the two matrices cannot be multiplied.
+ */
 struct matrix *mmult(const struct matrix *a, const struct matrix *b)
 {
-	// initialize matrix c to NULL pointer so it does not point to garbage
-	struct matrix *c = NULL;
+	struct matrix *c;
+
+	// pointers to the rows and columns in each 
+	int32_t *arow, *brow, *crow;
+	int32_t *acol, *bcol, *ccol;
+	
+	// checks if matrices have invalid or mismatched dimensions
+	if (a->m_rows < 1 || a->n_columns < 1 || b->m_rows < 1 || b->n_columns < 1) {
+		printf("ERROR: invalid matrix size\n");
+		return 0;
+	} else if (a->n_columns != b->m_rows) {
+		printf("ERROR: mismatched matrix sizes");
+		return 0;
+	}
+	
 	// set the correct size parameters for matrix c
 	c->m_rows = a->m_rows;
 	c->n_columns = b->n_columns;
+	
 	// create array of pointers for matrix c
-	c->row = calloc(c->m_rows, sizeof(int32_t));
-	// creates a set of columns for each row
-	for (int i = 0; i < (c->m_rows); i++)
-		(c->row + i) = calloc(c->n_columns, sizeof(int32_t));
+	c->array = (int32_t *)calloc(c->m_rows * c->n_columns, sizeof(int32_t));
 
+	// initialize the pointers to the beginning of each matrix
+	acol = arow = a->array;
+	bcol = brow = b->array;
+	ccol = crow = c->array;
+
+	// actually produces the multiplied matrix
+	for (int i = 0; i < (c->m_rows); i++) {
+		for (int j = 0; j < (c->n_columns); j++) {
+			for (int k = 0; k < (a->n_columns); k++)
+				*(c->column + j) += *(a->column + k) + *(b->row + k);
+			(c->column)++;
+			(b->column)++;
+		}
+		row += (c->n_columns);
+	}
 }
--- mmult.h
+++ mmult.h
@@ -2,16 +2,13 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
-
 #ifndef MMULT_H
 #define MMULT_H
 struct matrix {
 	// m_rows and n_columns are size parameters of the matrix
 	uint32_t m_rows;
 	uint32_t n_columns;
-	// *row and *column are pointers to an individual row and column
-	uint32_t *row;
-	uint32_t *column;
+	int32_t *array;
 }
 // multiplies two matricies a and b and returns a pointer to new matrix c
 struct matrix *mmult(const struct matrix *a, const struct matrix *b);
